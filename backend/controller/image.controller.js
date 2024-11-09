514f4024d56bc2c29b3a98e8050391cbee245877
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-11-09 16:10:09 +0545

    minor changes

--- backend/controller/image.controller.js
+++ backend/controller/image.controller.js
@@ -22,7 +22,7 @@ export const uploadImage = async (req, res) => {
 
 export const getImage = async (req, res) => {
   try {   
-    const file = await File.findById(request.params.fileId);
+    const file = await File.findById(res.params.fileId);
     
     file.downloadCount++;
 
