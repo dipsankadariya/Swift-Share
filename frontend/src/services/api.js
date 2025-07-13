1a8bfa88f25353ebcd4407145dbde08af0151efb
dipsankadariya <dipsankadariya99@gmail.com>
2025-07-13 09:45:36 +0545

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+const API_URL = 'https://swift-share-218z.onrender.com';
+
 export const uploadFile = async (data, onUploadProgress) => {
   try {
     const response = await axios.post(`${API_URL}/upload`, data, {
